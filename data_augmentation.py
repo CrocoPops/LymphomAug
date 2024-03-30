89e61c65bb37a37e05b9c91742316391ea374a18
eddyfri <eddy.frighetto@studenti.unipd.it>
2024-03-30 18:56:51 +0100

    start RGB rotation

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,8 +1,10 @@
 import albumentations
 import os
+import numpy as np
 import random
 from tqdm import tqdm
 import cv2
+import PIL
 
 # CONSTANTS
 BASE_FOLDER = os.getcwd()
@@ -12,6 +14,24 @@ IMAGE_FOLDERS = [
     os.path.join(BASE_FOLDER, "augmented", "2"), # IMAGE_FOLDERS[2] --> Image folder of class 2
 ]
 AUGMENTATIONS_PER_CLASS = 1000
+
+class RGBRotation(albumentations.ImageOnlyTransform):
+    def __init__(self, always_apply=False, p=0.5):
+        super(RGBRotation, self).__init__(always_apply, p)
+
+    def apply(self, image, **params):
+        image_array = np.array(image)
+        r, g, b = image_array[:, :, 0], image_array[:, :, 1], image_array[:, :, 2]
+
+        # rotate the RGB channels around each pixel in a clockwise direction
+        rotated_r = np.roll(r, shift=1, axis=0) # rotate the red channel downwards by one pixel
+        rotated_g = np.roll(g, shift=1, axis=1) # rotate the green channel downwards by one pixel
+        rotated_b = np.roll(b, shift=-1, axis=0) # rotate the blue channel upwards by one pixel
+
+        # merge the rotated RGB channels back into an image
+        rotated_image_array = np.dstack((rotated_r, rotated_g, rotated_b))
+
+        return PIL.Image.fromarray(rotated_image_array.astype('uint8'), 'RGB')
 
 # FUNCTIONS
 def select_random_file(folder_path):
@@ -26,6 +46,7 @@ augmentations = albumentations.Compose([
     albumentations.RandomRotate90(p=0.5),
     albumentations.RandomBrightnessContrast(p=0.5),
     albumentations.ShiftScaleRotate(p=0.5),
+    # RGBRotation(p=0.5)
 ])
 
 # AUGMENTING IMAGES
